ec4efb4e495fd4fcd521fe49540af9901d7f441b
Sam Beirne <22671090+sambeirne@users.noreply.github.com>
2019-12-31 17:12:12 -0800

    Build lua interpreter and compiler.

--- win64.cmd
+++ win64.cmd
@@ -15,11 +15,13 @@ if not exist bin md bin
 
 :lib
 rem SQLite
-cl /O2 /DSQLITE_API=__declspec(dllexport) /Fobuild\ lib\sqlite3\sqlite3.c /LD /link /dll /out:bin\sqlite3.dll /implib:build\sqlite3.lib
+cl /O2 /DSQLITE_API=__declspec(dllexport) /Fobuild\ lib\sqlite3\sqlite3.c /link /dll /out:bin\sqlite3.dll /implib:build\sqlite3.lib
 rem Lua
 set lua_files=lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lbitlib.c lcorolib.c ldblib.c liolib.c lmathlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c loadlib.c linit.c
 pushd lib\lua\src
-cl /O2 /DLUA_BUILD_AS_DLL /Fo..\..\..\build\ %lua_files% /LD /link /dll /out:..\..\..\bin\lua53.dll /implib:..\..\..\build\lua53.lib
+cl /O2 /DLUA_BUILD_AS_DLL /Fo..\..\..\build\ %lua_files% /link /dll /out:..\..\..\bin\lua53.dll /implib:..\..\..\build\lua53.lib
+cl /O2 /Fo..\..\..\build\ lua.c /link ..\..\..\build\lua53.lib /out:..\..\..\bin\lua.exe
+cl /O2 /Fo..\..\..\build\ luac.c %lua_files% /link /out:..\..\..\bin\luac.exe
 popd
 
 :fast
