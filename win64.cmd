1ae24860aff52eee9f0f7d06c4a141f33d39e0d4
Sam Beirne <22671090+sambeirne@users.noreply.github.com>
2020-01-04 15:28:03 -0800

    Dynamically link to CRT. Set optimize option for tlbx.exe.

--- win64.cmd
+++ win64.cmd
@@ -16,18 +16,18 @@ if not exist bin md bin
 :lib
 rem SQLite
 echo Building sqlite3...
-cl /O2 /DSQLITE_API=__declspec(dllexport) /Fobuild\ /nologo lib\sqlite3\sqlite3.c /link /dll /out:bin\sqlite3.dll /implib:build\sqlite3.lib
+cl /O2 /LD /MD /DSQLITE_API=__declspec(dllexport) /Fobuild\ /nologo lib\sqlite3\sqlite3.c /link /out:bin\sqlite3.dll /implib:build\sqlite3.lib
 rem Lua
 set lua_files=lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lbitlib.c lcorolib.c ldblib.c liolib.c lmathlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c loadlib.c linit.c
 pushd lib\lua\src
 echo Building lua library...
-cl /O2 /DLUA_BUILD_AS_DLL /Fo..\..\..\build\ /nologo %lua_files% /link /dll /out:..\..\..\bin\lua53.dll /implib:..\..\..\build\lua53.lib
+cl /O2 /LD /MD /DLUA_BUILD_AS_DLL /Fo..\..\..\build\ /nologo %lua_files% /link /out:..\..\..\bin\lua53.dll /implib:..\..\..\build\lua53.lib
 echo Building lua interpreter...
-cl /O2 /Fo..\..\..\build\ /nologo lua.c /link ..\..\..\build\lua53.lib /out:..\..\..\bin\lua.exe
+cl /O2 /MD /Fo..\..\..\build\ /nologo lua.c /link ..\..\..\build\lua53.lib /out:..\..\..\bin\lua.exe
 echo Building lua compiler...
-cl /O2 /Fo..\..\..\build\ /nologo luac.c %lua_files% /link /out:..\..\..\bin\luac.exe
+cl /O2 /MD /Fo..\..\..\build\ /nologo luac.c %lua_files% /link /out:..\..\..\bin\luac.exe
 popd
 
 :fast
 echo Building tlbx...
-cl /Fobuild\ /nologo src\*.c /link build\sqlite3.lib build\lua53.lib /out:bin\tlbx.exe
+cl /O2 /MD /Fobuild\ /nologo src\*.c /link build\sqlite3.lib build\lua53.lib /out:bin\tlbx.exe
