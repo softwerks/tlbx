4c51807de9e1e7f9bf9eedf3581d74015781f665
Sam Beirne <22671090+sambeirne@users.noreply.github.com>
2020-01-09 19:27:49 -0800

    Updated directory structure. Added license file.

--- win64.cmd
+++ win64.cmd
@@ -19,13 +19,13 @@ echo Building sqlite3...
 cl /MP /O2 /LD /MD /DSQLITE_API=__declspec(dllexport) /Fobuild\ /nologo lib\sqlite3\sqlite3.c /link /out:bin\sqlite3.dll /implib:build\sqlite3.lib
 rem Lua
 set lua_files=lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lbitlib.c lcorolib.c ldblib.c liolib.c lmathlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c loadlib.c linit.c
-pushd lib\lua\src
+pushd lib\lua
 echo Building lua library...
-cl /MP /O2 /LD /MD /DLUA_BUILD_AS_DLL /Fo..\..\..\build\ /nologo %lua_files% /link /out:..\..\..\bin\lua53.dll /implib:..\..\..\build\lua53.lib
+cl /MP /O2 /LD /MD /DLUA_BUILD_AS_DLL /Fo..\..\build\ /nologo %lua_files% /link /out:..\..\bin\lua53.dll /implib:..\..\build\lua53.lib
 echo Building lua interpreter...
-cl /MP /O2 /MD /Fo..\..\..\build\ /nologo lua.c /link ..\..\..\build\lua53.lib /out:..\..\..\bin\lua.exe
+cl /MP /O2 /MD /Fo..\..\build\ /nologo lua.c /link ..\..\build\lua53.lib /out:..\..\bin\lua.exe
 echo Building lua compiler...
-cl /MP /O2 /MD /Fo..\..\..\build\ /nologo luac.c %lua_files% /link /out:..\..\..\bin\luac.exe
+cl /MP /O2 /MD /Fo..\..\build\ /nologo luac.c %lua_files% /link /out:..\..\bin\luac.exe
 popd
 rem GLFW3
 set glfw3_files=context.c init.c input.c monitor.c vulkan.c window.c win32_init.c win32_joystick.c win32_monitor.c win32_time.c win32_thread.c win32_window.c wgl_context.c egl_context.c osmesa_context.c
